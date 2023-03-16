314402df8aa4350130c02f1cd0126997ab45e453
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-03-16 18:27:46 +0700

    membuka display

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -1,12 +1,9 @@
-// bubble_sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
-int main()
+
+
 int a[20];               //Deklarasi array a dengan ukuran 20 
 int n;                     //Deklarasi variasi n untuk menyimpan banyak elemen pada aray 
-
 
 void input() {            // prosedur untuk input 
 	while (true) {           // loping
@@ -18,3 +15,24 @@ void input() {            // prosedur untuk input
 			cout << "\nAray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
 		}
 	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) { // looping dengan 1 dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; // Output ke Layar
+		cin >> a[i];					// Input dari pengguna
+	}
+}
+void bubbleSortArray() {	//procedur untuk mengurutkan array dengan metode bubble}
+	for (int i = 1; i < n; i++) { //Looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) { //Looping dengan j dimulai dari 0 hingga n-i-1
+			if (a[j] > a[j + 1]) { // Jika nilai pada a{j} lebih besar dari a[j+l]
+				int temp = a[j];   //Simpan nilai a[j] ke variabel sementara tetap
+				a[j] = a[j + 1];   //assign nilai a[j+1] ke a[j]
+				a[j + 1] = temp;   //assogn nilai temp ke a[j+1]
+			}
+		}
+	}
+}
